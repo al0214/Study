89e38c641466bd9687dc4ee03f7c9d8e9afb8a4d
al0214 <sisang951@gmail.com>
2022-07-01 00:06:26 +0900

    2022/06/30/2

--- Bitcoin_use_Python/Pandas/DataFrame/7. Pandas_DataFrame_Exam.py
+++ Bitcoin_use_Python/Pandas/DataFrame/7. Pandas_DataFrame_Exam.py
@@ -1,3 +1,4 @@
+from matplotlib.pyplot import axis
 from pandas import *
 
 data = {"open" : [737, 750, 770],
@@ -7,6 +8,6 @@ data = {"open" : [737, 750, 770],
 df = DataFrame(data, index=["2018-01-01", "2018-01-02", "2018-01-03"])
 print(df)
 
-s = Series([55, 70, 20])
+s = Series([55, 70, 20], index=["2018-01-01", "2018-01-02", "2018-01-03"])
 df["volatility"] = s
 print(df)
